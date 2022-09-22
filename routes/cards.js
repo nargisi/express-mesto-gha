05b9990c3c7602d8cec2acca88104a4a28405c94
Nargisi <st-ana2005@yandex.ru>
2022-09-22 12:39:31 +0300

    Celebrate3

--- routes/cards.js
+++ routes/cards.js
@@ -9,7 +9,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().link(),
+    link: Joi.string().required(),
   }).unknown(true),
 }), createCard);
 router.delete('/:id', celebrate({
